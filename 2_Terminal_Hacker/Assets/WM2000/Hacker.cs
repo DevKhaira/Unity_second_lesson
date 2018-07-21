612f96a7e06da3ad78f51e7bd65b55b666fb4c4e
DevinderKhaira <devinderkhaira@hotmail.com>
2018-07-22 00:13:22 +0100

    push assets up

--- 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -4,9 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+    int level;
+    string chosenPassword;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen = Screen.MainMenu;
     string[] bears = { "Papa", "Mama", "Baby" };
+    string[,] passwords = new string[3, 5]  {{"fur", "teeth", "honey", "big", "small" },
+                                                {"panda", "leaves", "winter", "forest","porridge"},
+                                                { "autumn","multiplication","alphabet","hibernate","habitat" }} ;
     // Use this for initialization
     void Start () {
         string greeting = "Hello Meeshilocks";
@@ -31,35 +36,97 @@ public class Hacker : MonoBehaviour {
     // Update is called once per frame
     void OnUserInput (string input)
     {
-        switch (input) {
-            case "007":
-                Terminal.WriteLine("Meeshy Bond!");
-                Terminal.WriteLine("We've been expecting you!");
-                break;
-            case "menu":
-                showMainMenu("Hello Meeshikins");
-                break;
-            case "1":
-                startGame(1);
-                break;
-            case "2":
-                startGame(2);
-                break;
-            case "3":
-                startGame(3);
-                break;
-            default:
-                 Terminal.WriteLine("Meeshy that's the wrong selection:(");
-                 Terminal.WriteLine("Did you boobs get in the way?");
-                 break;
+        if (currentScreen == Screen.MainMenu)
+        {
+            switch (input)
+            {
+                case "007":
+                    Terminal.WriteLine("Meeshy Bond!");
+                    Terminal.WriteLine("We've been expecting you!");
+                    break;
+                case "menu":
+                    showMainMenu("Hello Meeshikins");
+                    break;
+                case "1":
+                    StartGame(1);
+                    break;
+                case "2":
+                    StartGame(2);
+                    break;
+                case "3":
+                    StartGame(3);
+                    break;
+                default:
+                    Terminal.WriteLine("Meeshy that's the wrong selection:(");
+                    Terminal.WriteLine("Did you boobs get in the way?");
+                    break;
             }
+        } else if (currentScreen == Screen.Password)
+        {
+            checkPassword(input);
+        }
+          else
+        {
+            showMainMenu("Meeshy-poo");
+        }
+    }
+    private string selectPassword (int level)
+    {
+        System.Random random = new System.Random();
+        return passwords[level, random.Next(1, 5)];
     }
 
-    private void startGame(int input)
+    private string scramblePassword (string password)
+    {
+        int length = password.Length;
+        System.Random random = new System.Random();
+        System.Text.StringBuilder scramble = new System.Text.StringBuilder();
+        scramble.Append(password);
+        print(password);
+        for (int i = 0; i < password.Length; i++)
+        {
+            int j = random.Next(password.Length);
+            char temp = scramble[i];
+            print(scramble + "first");
+            scramble[i] = scramble[j];
+            print(scramble + "second");
+            scramble[j] = temp;
+            print(scramble + "third");
+
+        }
+        print(scramble);
+        return scramble.ToString();
+    }
+
+    private void checkPassword (string input)
+    {
+        bool victory = false;
+        if (input == chosenPassword)
+            {
+                victory = true;
+            }
+        if (victory == true)
+        {
+            currentScreen = Screen.Win;
+            Terminal.ClearScreen();
+            Terminal.WriteLine("Well Done Meeshy, I love you!");
+        } else
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("guess again gorgeous");
+            Terminal.WriteLine("You have chosen " + bears[level - 1] + "'s room");
+            Terminal.WriteLine("Hint: " + scramblePassword(chosenPassword));
+        }
+    }
+
+    private void StartGame(int input)
     {
         currentScreen = Screen.Password;
+        level = input;
         Terminal.WriteLine("You have chosen " + bears[input - 1] + "'s room");
         Terminal.WriteLine("Please enter the password to get in!");
+        chosenPassword = selectPassword(level - 1);
+        Terminal.WriteLine("Hint: " + scramblePassword(chosenPassword));
     }
 
     void Update () {
